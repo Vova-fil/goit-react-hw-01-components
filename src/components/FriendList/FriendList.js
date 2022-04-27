bec301de84ada042664a9aa831d8123cd8a0ae5e
Vova-fil <vfil.zp@gmail.com>
2022-04-27 04:16:50 +0300

    fix 4

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -20,10 +20,10 @@ export function FriendList({ friends }) {
 FriendList.propTypes = {
   friends: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+      name: PropTypes.number.isRequired,
       avatar: PropTypes.string.isRequired,
-      isOnline: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
     })
   ),
 };
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import s from './Statistics.module.css';
 
-export function Statistics({ stats, title = true }) {
+export function Statistics({ stats, title }) {
   return (
     <section className={s.section}>
-      {title && <h2 className={s.title}>Upload stats</h2>}
+      {title && <h2 className={s.title}>{title}</h2>}
 
       <ul className={s.statList}>
         {stats.map(({ id, label, percentage }) => (
